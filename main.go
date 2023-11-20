da38cdaa99841b4a1fbc3e8283a41ef0d59d9342
DenysWinPC <denkochev78@gmail.com>
2023-11-20 18:11:26 +0100

    feat: implement example of usage

--- main.go
+++ main.go
@@ -6,24 +6,27 @@ import (
 )
 
 func main() {
-	// pring secp256k1 G coordinates
-	wrapper.PrintBasicG()
+	// get G point of secp256k1 curve and print it
+	G_point := wrapper.GetBasicG()
+	G_point.PrintPoint()
+	// check if point is on secp256k1 curve [always true]
+	fmt.Println("Is G on point -> ", G_point.IsOnCurveCheck())
 
+	// create random point and check if it's on curve [should be false with very high percentile]
 	x, _ := wrapper.GenerateRandomBigInt(256)
 	y, _ := wrapper.GenerateRandomBigInt(256)
+	rand_point := wrapper.SetNewPoint(x, y)
+	fmt.Println("Is random point belongs to secp256k1 curve ? - > ", rand_point.IsOnCurveCheck())
 
-	point := wrapper.SetNewPoint(x, y)
-	// check if our random point on secp256k1
-	fmt.Println("Is point on secp256k1 curve ? - > ", point.IsOnCurveCheck())
-
-	// get random point on secp256k1
-	EC, err := wrapper.GetRandomECpoint()
+	// get random point ON secp256k1
+	EC_point, err := wrapper.GetRandomECpoint()
 	if err != nil {
 		fmt.Println(err)
 	}
 	// should always be true
-	fmt.Println("Is EC on secp256k1 curve ? - > ", EC.IsOnCurveCheck())
+	fmt.Println("Is EC_point belongs to secp256k1 curve ? - > ", EC_point.IsOnCurveCheck())
 
+	// create two points and do Group operations with them
 	A_point, _ := wrapper.GetRandomECpoint()
 	B_point, _ := wrapper.GetRandomECpoint()
 	// add two point
@@ -33,5 +36,13 @@ func main() {
 	// get double from point
 	AB_double := wrapper.Double(AB_point)
 	AB_double.PrintPoint()
-	fmt.Println(AB_double.IsOnCurveCheck())
+	fmt.Println("Is doubled A+B point belongs to the curve? -> ", AB_double.IsOnCurveCheck())
+
+	// multiply AB_double point on scalar
+	scalar, _ := wrapper.GenerateRandomBigInt(256)
+
+	AB_double_mul_scalar_point := wrapper.ScalarMult(AB_double, scalar)
+	// check of AB_double_scalar_point belongs to the curve
+	fmt.Println("Is (doubled AB) * scalar belongs to the curve -> ", AB_double_mul_scalar_point.IsOnCurveCheck())
+
 }
